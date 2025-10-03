e9da239b87df65692b02ebbdf58ba0a97b1dc4c5
carlosrobles666 <133397390+CarlosRobles666@users.noreply.github.com>
2025-10-03 11:53:29 -0500

    Add files via upload

---
+++ models.py
@@ -0,0 +1,34 @@
+from flask_login import UserMixin
+from db import get_db_connection
+
+class User(UserMixin):
+    def __init__(self, id, email, password):
+        self.id = id
+        self.email = email
+        self.password = password
+
+    @staticmethod
+    def get_by_email(email):
+        # consulta a la tabla usuarios
+        conn = get_db_connection()
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("select * from usuarios where email=%s", (email,))
+        usuario = cursor.fetchone()
+        cursor.close()
+        conn.close()
+        if usuario:
+            return User(usuario["idusuario"], usuario["email"], usuario["password"])
+        return None
+
+    @staticmethod
+    def get_by_id(user_id):
+        # consulta por id
+        conn = get_db_connection()
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("select * from usuarios where id=%s", (user_id,))
+        usuario = cursor.fetchone()
+        cursor.close()
+        conn.close()
+        if usuario:
+            return User(usuario["idusuario"], usuario["email"], usuario["password"])
+        return None
